164f055efd93fdcd5694e96674cca74f3ef0675f
Bob Follek <bfollek@gmail.com>
2019-03-30 16:33:49 -0400

    Day01.Part2 done ATP

--- cmd/advent2018go/day01.go
+++ cmd/advent2018go/day01.go
@@ -24,6 +24,34 @@ func Part1(fileName string) (int, error) {
 	return sum, nil
 }
 
+// Part2 finds the first repeated frequency.
+func Part2(fileName string) (int, error) {
+	sum := 0
+	seen := map[int]bool{sum: true}
+	numsAsStrings, err := loadData(fileName)
+	if err != nil {
+		return 0, err
+	}
+	nums := []int{}
+	for _, s := range numsAsStrings {
+		var i int
+		if i, err = strconv.Atoi(s); err != nil {
+			return 0, err
+		}
+		nums = append(nums, i)
+	}
+	// We may have to cycle through the frequencies more than once
+	for {
+		for _, n := range nums {
+			sum += n
+			if seen[sum] {
+				return sum, nil
+			}
+			seen[sum] = true
+		}
+	}
+}
+
 func loadData(fileName string) ([]string, error) {
 	absPath, err := filepath.Abs(fileName)
 	if err != nil {
--- cmd/advent2018go/day01_test.go
+++ cmd/advent2018go/day01_test.go
@@ -14,3 +14,14 @@ func TestPart1(t *testing.T) {
 		t.Errorf("Error - expected [%d], got [%d]", expected, result)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	expected := 241
+	result, err := Part2("../../data/day01.txt")
+	if err != nil {
+		t.Errorf("Unexpected error - [%s]", err)
+	}
+	if expected != result {
+		t.Errorf("Error - expected [%d], got [%d]", expected, result)
+	}
+}
