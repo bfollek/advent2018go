a8f2b29b25435da1d641295616972b56fd6ee11f
Bob Follek <bfollek@gmail.com>
2019-03-31 21:04:57 -0400

    Day02.Part2 working ATP ALP

--- cmd/advent2018go/day02/day02.go
+++ cmd/advent2018go/day02/day02.go
@@ -1,6 +1,8 @@
 package day02
 
 import (
+	"bytes"
+
 	"github.com/bfollek/advent2018go/cmd/advent2018go/util"
 )
 
@@ -40,4 +42,46 @@ func checkID(id string) (bool, bool) {
 		}
 	}
 	return has2, has3
+}
+
+// Part2 finds the two box IDs that differ by just one char,
+// and returns their common chars.
+func Part2(fileName string) string {
+	ids := util.MustLoadData(fileName)
+	for _, id1 := range ids {
+		for _, id2 := range ids {
+			if b, commonChars := differByOne(id1, id2); b {
+				return commonChars
+			}
+		}
+	}
+	return ""
+}
+
+// The second value returned is a string of the chars
+// that s1 and s2 have in common. It's meaningful only
+// if the first value is true.
+func differByOne(s1, s2 string) (bool, string) {
+	var commonChars bytes.Buffer
+	var diffCnt int
+	// Work with rune slices so the indexes are reliable.
+	r1 := []rune(s1)
+	r2 := []rune(s2)
+	if len(r1) != len(r2) {
+		return false, ""
+	}
+	for idx, val := range r1 {
+		if val != r2[idx] {
+			diffCnt++
+			if diffCnt > 1 {
+				return false, ""
+			}
+		} else {
+			commonChars.WriteRune(val)
+		}
+	}
+	if diffCnt == 1 {
+		return true, commonChars.String()
+	}
+	return false, ""
 }
