584fd1c870b4593285a7c9148587a257e74e8a5c
Bob Follek <bfollek@gmail.com>
2019-04-06 14:24:19 -0400

    Day04.Part1 works ATP

--- cmd/advent2018go/day04/day04.go
+++ cmd/advent2018go/day04/day04.go
@@ -34,9 +34,9 @@ func init() {
 // that minute multiplied by the guard's ID.
 func Part1(fileName string) int {
 	m := mapData(fileName)
-	_ = m
-	//todo convert id to string
-	return 902
+	id := mostMinutesAsleep(m)
+	min := minuteMostAsleep(m[id])
+	return util.MustAtoi(id) * min
 }
 
 func mapData(fileName string) map[string]minutes {
@@ -66,4 +66,34 @@ func logNap(m map[string]minutes, id string, fallsAsleep, wakesUp string) {
 		mins[i]++
 		m[id] = mins
 	}
+}
+
+// Return the id of the guard who spends the most minutes asleep.
+func mostMinutesAsleep(m map[string]minutes) string {
+	maxSoFar := -1
+	maxSoFarID := ""
+	for id, mins := range m {
+		sum := 0
+		for _, n := range mins {
+			sum += n
+		}
+		if sum > maxSoFar {
+			maxSoFar = sum
+			maxSoFarID = id
+		}
+	}
+	return maxSoFarID
+}
+
+// Given a minutes type, return the minute where the guard was most asleep.
+func minuteMostAsleep(mins minutes) int {
+	maxSoFar := -1
+	maxSoFarMin := -1
+	for i, n := range mins {
+		if n > maxSoFar {
+			maxSoFar = n
+			maxSoFarMin = i
+		}
+	}
+	return maxSoFarMin
 }
