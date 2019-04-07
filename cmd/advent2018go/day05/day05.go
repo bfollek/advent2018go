d3d4078ba302807ff2d393d96e2f8661b0b2f538
Bob Follek <bfollek@gmail.com>
2019-04-07 08:51:03 -0400

    Refactored some names ATP ALP

--- cmd/advent2018go/day05/day05.go
+++ cmd/advent2018go/day05/day05.go
@@ -23,22 +23,22 @@ func runReaction(polymer string) string {
 	if polymer == "" {
 		return "" // Guard against edge case
 	}
-	polyRunes := []rune(polymer)
-	rv := []rune{polyRunes[0]}
-	for i := 1; i < len(polyRunes); i++ {
-		next := polyRunes[i]
+	units := []rune(polymer)
+	rv := []rune{units[0]}
+	for i := 1; i < len(units); i++ {
+		nextUnit := units[i]
 		lastIndex := len(rv) - 1
-		last := rv[lastIndex]
-		if react(next, last) {
-			rv = rv[:lastIndex] // Drop last from rv
+		lastUnit := rv[lastIndex]
+		if react(nextUnit, lastUnit) {
+			rv = rv[:lastIndex] // Drop last unit from rv
 		} else {
-			rv = append(rv, next) // Add next to rv
+			rv = append(rv, nextUnit) // Add next unit to rv
 		}
 	}
 	return string(rv)
 }
 
-func react(r1, r2 rune) bool {
+func react(unit1, unit2 rune) bool {
 	// Same char, different cases, e.g. a, A
-	return (r1 != r2) && (unicode.ToUpper(r1) == unicode.ToUpper(r2))
+	return (unit1 != unit2) && (unicode.ToUpper(unit1) == unicode.ToUpper(unit2))
 }
