04d0f82bb6c74ca47ad6d5c99eb8d05d16171515
Bob Follek <bfollek@gmail.com>
2019-04-07 08:43:48 -0400

    Day05.Part1 works ATP ALP

--- cmd/advent2018go/day05/day05.go
+++ cmd/advent2018go/day05/day05.go
@@ -1,11 +1,44 @@
 package day05
 
+import (
+	"unicode"
+
+	"github.com/bfollek/advent2018go/cmd/advent2018go/util"
+)
+
+// Part1 returns the length of the reacted polymer.
 func Part1(fileName string) int {
-	_ = fileName
-	return 1
+	polymer := util.MustLoadString(fileName)
+	reacted := runReaction(polymer)
+	return len(reacted)
 }
 
+// Part2 returns the length of the improved polymer.
 func Part2(fileName string) int {
 	_ = fileName
 	return 2
+}
+
+func runReaction(polymer string) string {
+	if polymer == "" {
+		return "" // Guard against edge case
+	}
+	polyRunes := []rune(polymer)
+	rv := []rune{polyRunes[0]}
+	for i := 1; i < len(polyRunes); i++ {
+		next := polyRunes[i]
+		lastIndex := len(rv) - 1
+		last := rv[lastIndex]
+		if react(next, last) {
+			rv = rv[:lastIndex] // Drop last from rv
+		} else {
+			rv = append(rv, next) // Add next to rv
+		}
+	}
+	return string(rv)
+}
+
+func react(r1, r2 rune) bool {
+	// Same char, different cases, e.g. a, A
+	return (r1 != r2) && (unicode.ToUpper(r1) == unicode.ToUpper(r2))
 }
--- cmd/advent2018go/day05/day05_test.go
+++ cmd/advent2018go/day05/day05_test.go
@@ -2,6 +2,15 @@ package day05
 
 import "testing"
 
+func TestRunReaction(t *testing.T) {
+	input := "dabAcCaCBAcCcaDA"
+	expected := "dabCBAcaDA"
+	result := runReaction(input)
+	if expected != result {
+		t.Errorf("Error - expected [%s], got [%s]", expected, result)
+	}
+}
+
 func TestPart1(t *testing.T) {
 	expected := 10450
 	result := Part1("testdata/day05.txt")
@@ -10,10 +19,10 @@ func TestPart1(t *testing.T) {
 	}
 }
 
-func TestPart2(t *testing.T) {
-	expected := 4624
-	result := Part2("testdata/day05.txt")
-	if expected != result {
-		t.Errorf("Error - expected [%d], got [%d]", expected, result)
-	}
-}
+// func TestPart2(t *testing.T) {
+// 	expected := 4624
+// 	result := Part2("testdata/day05.txt")
+// 	if expected != result {
+// 		t.Errorf("Error - expected [%d], got [%d]", expected, result)
+// 	}
+// }
